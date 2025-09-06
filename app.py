d9f9a44944b7e0103c3505c6c3e9d27f317053df
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2025-09-07 05:03:16 +0800

    Reimplemented the whole app in playwright instead of selenium and bs4

--- app.py
+++ app.py
@@ -1,41 +1,24 @@
 # Built-in dependencies
 import os                         # Handling file operations
 import re                         # Filename curing (regex)
-import time                       # Handling sleep (await)
 from pathlib import Path          # Handling file operations
+import time
 import datetime                   # Naming folders inside `saved` directory
 from typing import List, Literal  # Type annotations
 
 # External dependencies
-import typer                                           # CLI tools
-import requests                                        # Downloading files
-from bs4 import BeautifulSoup                          # Parsing HTML
-from selenium import webdriver                         # Simulating browser (access YTMP3 website)
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from rich import print                                 # Rich printing with color support
+import typer                                           # CLI application tools
+import requests                                        # Downloading file
+from playwright.sync_api import sync_playwright        # For scraping YTMP3.as
+from rich import print                                 # Printing with color support
 from rich.progress import Progress                     # Progress bar
-
+from rich.progress import MofNCompleteColumn, SpinnerColumn, TextColumn, BarColumn, TotalFileSizeColumn, TimeElapsedColumn
 
 """PART A: BACKEND AND CLI-UI FUNCTIONS"""
 
 
 DEBUG_MODE = False      # If set to True, Debug Mode will open webdriver without --headless-mode (browser will be visible)
 DOWNLOAD_DIR = "saved"  # Default folder location for video downloads
-
-
-def create_web_driver():
-  """Create a new configured Chrome web driver for browsing"""
-
-  # Connect to existing instance
-  options = Options()
-
-  if not DEBUG_MODE:
-    options.add_argument("--headless")
-
-  driver = webdriver.Chrome(options=options)
-
-  return driver
 
 
 def parse_video_id_from_link(link: str) -> str:
@@ -47,155 +30,214 @@ def parse_video_id_from_link(link: str) -> str:
 def parse_video_ids_from_playlist(playlist_link: str) -> List[str]:
   """Parse all video ids from playlist"""
 
-  driver = create_web_driver()
+  with sync_playwright() as p:
+    
+    browser = p.firefox.launch(headless=True)
 
-  driver.get(playlist_link)  # Get playlist page
-  time.sleep(1)              # Ensure YouTube has loaded properly
-  
-  bs = BeautifulSoup(driver.page_source, "html.parser")
+    page = browser.new_page()
 
-  # Get all anchors
-  anchors = bs.find_all("a", { "class": "yt-simple-endpoint style-scope ytd-playlist-video-renderer" })
-  
-  video_ids = []
+    page.goto(playlist_link)
 
-  # Filter anchors for only related links to playlist
-  for a in anchors:
-    if "/watch?v=" in a.get("href"):
-      video_ids.append(parse_video_id_from_link(a.get("href")))
+    # Ensure the whole page is fully loaded
+    page.wait_for_load_state("networkidle")
 
-  return video_ids
+    video_ids = page.evaluate("""
+      () => {
+          const anchors = Array.from(document.querySelectorAll('a.yt-simple-endpoint'));
+          const links = anchors
+                          .map(link => link.href)
+                          .filter(href => href.includes('watch?v='))
+                          .map(href => {
+                              const url = new URL(href);
+                              return url.searchParams.get('v');
+                          })
+                          .filter(id => id); // Remove nulls
+          return [...new Set(links)]  // Remove duplicates
+      }
+    """)
+
+    return video_ids
 
 
 def parse_video_ids_from_html(path_to_playlist_html: Path) -> List[str]:
   """Parse all video ids from playlist in HTML file"""
   
-  # Get HTML content
+  # Read HTML file
   with open(path_to_playlist_html, "r", encoding="utf-8") as f:
+
     content = f.read()
 
-  soup = BeautifulSoup(content, "html.parser")
+  with sync_playwright() as p:
+    
+    browser = p.firefox.launch(headless=True)
 
-  # Find the parent div containing all playlist links
-  div = soup.find("div", { "id": "items", "class": "playlist-items style-scope ytd-playlist-panel-renderer"})
-  links = div.find_all("a")
+    page = browser.new_page()
 
-  # Remove duplicates or unrelated links
-  video_ids = []
-  for l in links:
-    if l.get("href", "") != "" and l["href"].find("https://www.youtube.com/watch?v=") != -1:
-      parse_l = parse_video_id_from_link(l["href"])
-      if not (parse_l in video_ids):
-        video_ids.append(parse_l)  
+    page.set_content(content)
+    
+    video_ids = page.evaluate("""
+      () => {
+          const anchors = Array.from(document.querySelectorAll('a.yt-simple-endpoint'));
+          const temp = anchors
+                          .map(link => link.href)
+                          .filter(href => href.includes('watch?v='))
+                          .filter(href => href.includes('&'))
+          const playlist_link = new URL(temp[0]).searchParams.get('list');
+          const links = temp
+                          .filter(href => href.includes(playlist_link))
+                          .map(href => {
+                              const url = new URL(href);
+                              return url.searchParams.get('v');
+                              })
+          return [...new Set(links)]  // Remove duplicates
+      }
+    """)
 
-  return video_ids
+    return video_ids
 
 
 def download_videos(video_ids: List[str], format: Literal["mp3", "mp4"], download_dir: Path, is_silent: bool=False) -> None:
   """Download all videos from playlist in MP3/MP4 format with progress bar"""
   
-  # Create directory if it doesn't exist
+  # Create directory regardless of its prior existence
   download_dir.mkdir(parents=True, exist_ok=True)
   
-  driver = create_web_driver()
+  print()
 
-  illegal_char_replacement_flag = False
-  invalid_links = []
+  with Progress(SpinnerColumn(), TextColumn("[progress.description]{task.description}")) as progressbar, sync_playwright() as p:
+    
+    task = progressbar.add_task(f"Preparing [bright_yellow]{len(video_ids)} [white]downloads in [bright_yellow]{format} [white]to [bright_yellow]{download_dir}", total=1)
 
-  for video_id in video_ids:
-    if len(video_id) != 11:
-      invalid_links.append(f"https://youtube.com/watch?v={video_id}")
-      continue
+    browser = p.firefox.launch(headless=True)
 
-    # Open YTMP3 website
-    driver.get("https://ytmp3.as/cyPH/")
+    page = browser.new_page()
 
-    is_mp3 = format.lower() == "mp3"
+    illegal_char_replacement_flag = False
+    invalid_links = []
 
-    if is_mp3:
-      # Override download method inside the JS script to get the download link in Python
-      driver.execute_script("""
-        window.download = function (e, t, r, n) {
-          window._downloadLink = e;
-          window._title = n;
-        };
-      """)
-    else:
-      driver.execute_script("""
-        window.download = function (e, t, r, n) {
-          window._downloadLink = e;
-        };
-      """)
+    for video_id in video_ids:
 
-    # Enter YT link in the input element
-    link_input_box = driver.find_element(By.ID, "v")
-    link_input_box.send_keys(f"https://youtube.com/watch?v={video_id}")
+      # Goto YTMP3 website
+      page.goto("https://ytmp3.as/cyPH/")
 
-    # Press the submit button
-    format_button = driver.find_element(By.XPATH, "//button[1]")  # Default: MP3
+      if not progressbar.finished:
+        progressbar.update(task, advance=1, description=f"[bright_green]Download process ready (Location: {download_dir})", completed=True)
+        progressbar.stop()
 
-    if not is_mp3:
-      format_button.click()  # Change to MP4 by clicking
+      # Flag for handle MP3 or MP4
+      is_mp3 = format.lower() == "mp3"
 
-    # Press the convert button
-    convert_button = driver.find_element(By.XPATH, "//button[2]")
-    convert_button.click()
+      if is_mp3:
 
-    # Wait until the download link has been created
-    while not driver.execute_script("return window._downloadLink"):
-      try:
-        if driver.find_element(By.XPATH, "//button[1]"):
-          break
-      except:
+        # Override default download method from the website to gain download link
+        page.evaluate("""
+          window.download = function (e, t, r, n) {
+            window._downloadLink = e;
+            window._title = n;
+          };
+        """)
+
+      else:
+
+        # Override default download method from the website to gain download link and video title
+        page.evaluate("""
+          window.download = function (e, t, r, n) {
+            window._downloadLink = e;
+          };
+        """)
+
+      # Enter YouTube link into input element
+      link_input_box = page.query_selector("#v")
+      link_input_box.fill(f"https://youtube.com/watch?v={video_id}")
+
+      # Button for specifying download format (MP3 or MP4)
+      format_button = page.locator("button").first 
+
+      if not is_mp3:
+        format_button.click()  # Change to MP4 by simulating clicking button
+
+      # Click convert button to start conversion process
+      convert_button = page.locator("button").last
+      convert_button.click()
+
+      # Wait until the download link has been created
+      while page.evaluate("""() => window._downloadLink""") == None:
+
+        try:
+
+          # Check if there's error happening on the website
+          # When the number of button is only 1, it means that an error has occurred
+          # (Try it yourself by entering a non-existent YouTube video link and see what happens at ytmp3.as)
+          if len(page.query_selector_all("button")) == 1:
+            break
+          
+        except:
+          continue
+
+      # Get download link
+      download_link = page.evaluate("""() => window._downloadLink""")
+
+      # Case where conversion failed, continue to next video (while keeping track of unsuccessful links)
+      if download_link == None:
+        
+        invalid_links.append(f"https://youtube.com/watch?v={video_id}")
         continue
+        
+      title = ""
+      if is_mp3:
+        title = page.evaluate("""() => window._title""")
+      else:
+        title = page.locator("div").first.text_content()
 
-    # Grab the download link
-    download_link = driver.execute_script("return window._downloadLink")
+      # Setup requests to mimic browser behavior for file downloads
+      headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': '*/*',
+        'Accept-Encoding': 'identity',  # Important for progress tracking
+        'Connection': 'keep-alive',
+      }
 
-    if not download_link:
-      invalid_links.append(f"https://youtube.com/watch?v={video_id}")
-      continue
+      # Replace illegal characters from video title to avoid file system errors
+      filename = re.sub(r'[/\\*?"<>|:]', "_", title.strip())
+      download_path = os.path.join(download_dir, f"{filename}.{format}")
 
-    title = ""
-    if is_mp3:
-      title = driver.execute_script("return window._title")
-    else:
-      title = driver.find_element(By.XPATH, "//form/div[1]").text
+      # Get the file
+      response = requests.get(download_link, headers=headers, stream=True)
+      response.raise_for_status()
 
-    # Mimic browser behavior
-    headers = {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-      'Accept': '*/*',
-      'Accept-Encoding': 'identity',  # Important for progress tracking
-      'Connection': 'keep-alive',
-    }
+      # File metadata
+      total_size = int(response.headers.get('content-length', 0))
+      block_size = 1024  # 1 Kibibyte
+      file_size = total_size / (1024*1024)
 
-    response = requests.get(download_link, headers=headers, stream=True)
-    response.raise_for_status()
+      if is_silent and (not illegal_char_replacement_flag) and (filename != title.strip()):
 
-    total_size = int(response.headers.get('content-length', 0))
-    block_size = 1024  # 1 Kibibyte
-    file_size = total_size / (1024*1024)
+        illegal_char_replacement_flag = True
+        print("[bright_green]Note: [white]Illegal characters such as '([*?])' are replaced with '_' inside filenames")
 
-    # Cure filename by replacing illegal characters with '_'
-    filename = re.sub(r'[/\\*?"<>|:]', "_", title.strip())
+      print()
 
-    if is_silent and (not illegal_char_replacement_flag) and (filename != title.strip()):
-      illegal_char_replacement_flag = True
-      print("[chartreuse1]Note: [white]Illegal characters such as '([*?])' are replaced with '_' inside filenames")
+      # Progress bar for download progress feedback
+      with open(download_path, 'wb') as file, Progress(SpinnerColumn(), TextColumn("[progress.description]{task.description}"), BarColumn(), TotalFileSizeColumn(), TimeElapsedColumn(), disable=is_silent) as progressbar:
 
-    download_path = os.path.join(download_dir, f"{filename}.{format}")
+        task = progressbar.add_task(description=f'Downloading [yellow]{title}', total=total_size, visible=not is_silent)
+        
+        for data in response.iter_content(block_size):
 
-    with open(download_path, 'wb') as file, Progress(disable=is_silent) as progressbar:
-      task = progressbar.add_task(description=f'Downloading [yellow]{title} [white]({file_size:.2f}MB)', total=total_size, visible=not is_silent)
-      for data in response.iter_content(block_size):
-        progressbar.update(task, advance=len(data))
-        file.write(data)
+          progressbar.update(task, advance=len(data))
+          file.write(data)
+      
+        progressbar.update(task, description=f'[bright_green]Successful: {filename}.{format}', total=total_size, visible=not is_silent)
 
   # Display download summary
   print(f"\n[green]{len(video_ids) - len(invalid_links)} [white]videos downloaded successfully. [bright_red]{len(invalid_links)} [white]invalid links found.")
   "" if len(invalid_links) == 0 else print(f"\n[bright_red]Invalid links: [white]", end="")
   print(*invalid_links, sep=", ")
+
+
+def is_valid_video_id(video_id: str) -> bool:
+  """Determine whether the video id is correct syntatically"""
+  return len(video_id) == 11
 
 
 def get_next_folder_number() -> int:
@@ -254,23 +296,56 @@ def download(
 
   video_ids = []
 
-  print("\nParsing the video links...")
+  # 1. Extract YouTube video links
+  with Progress(SpinnerColumn(), TextColumn("[progress.description]{task.description}")) as progressbar:
+    
+    print()
+    type_str = "playlist"
+    task = progressbar.add_task(description=f"Finding {type_str} videos from [bright_yellow]{link_or_path}", total=1)
 
-  # Determine whether input is a video link, playlist link or HTML file
-  if ".html" in link_or_path:  # HTML file
-    video_ids = parse_video_ids_from_html(link_or_path)
+    # Determine whether input is a video link, playlist link or HTML file
+    if ".html" in link_or_path:  # HTML file
+      
+      video_ids = parse_video_ids_from_html(link_or_path)
 
-  elif "v=" in link_or_path:  # Video link
-    video_ids.append(parse_video_id_from_link(link_or_path))
+    elif "v=" in link_or_path:  # Video link
+      type_str = "video"
+      video_ids.append(parse_video_id_from_link(link_or_path))
 
-  elif "list=" in link_or_path:  # Playlist link
-    video_ids = parse_video_ids_from_playlist(link_or_path)
-  
-  if len(video_ids) == 0:
-    print("[bright_red]Invalid link or path")
+    elif "list=" in link_or_path:  # Playlist link
+      video_ids = parse_video_ids_from_playlist(link_or_path)
+    
+    if len(video_ids) == 0:
+      progressbar.update(task, description=f"[bright_red]Error: Can't find any {type_str} from {link_or_path}", advance=1)
+      print("[bright_red][bold]Please provide a valid link or path")
+      return
+    
+    progressbar.update(task, description=f"[bright_green]Found {len(video_ids)} video" + ("s" if len(video_ids) > 1 else ""), advance=1)
+    
+  print()
 
-  else:
-    download_videos(video_ids, format, download_location, is_silent)
+  # 2. Verifying links
+  with Progress(SpinnerColumn(), TextColumn("[progress.description]{task.description}"), MofNCompleteColumn()) as progressbar:
+    
+    task = progressbar.add_task("Validating video IDs", total=len(video_ids))
+    
+    invalids = []
+    for v in video_ids:
+      progressbar.update(task, advance=1, description="Validating video IDs")
+      if not is_valid_video_id(v):
+        invalids.append(v)
+
+    time.sleep(1.3)
+
+    valid_count = len(video_ids) - len(invalids)
+    progressbar.update(task, description=f"[bright_green]{valid_count} valid ID found. " + (f"[bright_red]{len(invalids)} [white]invalid ID found." if len(invalids) > 0 else ""))
+
+    if invalids:
+      print("[bright_red]Invalid links: ", end="")
+      print(*[f"[grey74]https://youtube.com/watch?v={id}" for id in invalids], sep="[white], ")
+
+  # 3. Download videos
+  download_videos(video_ids, format, download_location, is_silent)
 
 
 if __name__ == "__main__":
